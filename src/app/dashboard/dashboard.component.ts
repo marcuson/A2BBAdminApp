a2b4f32426a3bce9719cfea2248403d8b1c33719
Leonardo Marcucci <l.marcucci@sintraconsulting.eu>
2017-02-27 12:54:15 +0100

    Add logic and UI elements to manage granters.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { A2BBAuthService } from '../services/a2bb-auth.service';
 import { Router } from '@angular/router';
 import { User } from '../models/user';
+import { Granter } from '../models/granter';
 import { FormGroup } from '@angular/forms';
 import { Const } from '../const';
 
@@ -12,25 +13,43 @@ import { Const } from '../const';
 })
 export class DashboardComponent implements OnInit {
   users: User[];
+  granters: Granter[];
   newUserName: string;
   newUserPass: string;
+  newGranterId: string;
   selectedUser: User;
+  selectedGranter: Granter;
   newUserInfo: string;
-  prova: any;
+  newGranterInfo: string;
+  linkGranterInfo: string;
 
   constructor(private _router: Router, private _a2bbAuthService: A2BBAuthService) { }
 
   ngOnInit() {
     this.refreshUsers();
+    this.refreshGranters();
   }
 
   selectUser(user: User) {
     this.selectedUser = user;
   }
 
+  selectGranter(granter: Granter) {
+    this.selectedGranter = granter;
+  }
+
   refreshUsers(): void {
     this._a2bbAuthService.get(Const.ID_SRV_ENDPOINT + '/api/admin/users').then((res) => {
       this.users = res.json() as User[];
+    }).catch((err) => {
+      console.log(err);
+      this._router.navigate(['/login']);
+    });
+  }
+
+  refreshGranters(): void {
+    this._a2bbAuthService.get(Const.API_ENDPOINT + '/api/admin/granter').then((res) => {
+      this.granters = res.json().payload as Granter[];
     }).catch((err) => {
       console.log(err);
       this._router.navigate(['/login']);
@@ -72,7 +91,53 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  deleteSelected(): void {
+  linkGranter(): void {
+    this.selectedGranter.subId = this.selectedUser.id;
+
+    this._a2bbAuthService.put(Const.API_ENDPOINT + '/api/admin/granter/' + this.selectedGranter.id, this.selectedGranter).then((res) => {
+      const response = res.json();
+
+      if (response.code === 0) {
+        this.linkGranterInfo = 'Ok!';
+        return this.refreshGranters();
+      }
+
+      this.linkGranterInfo = 'Error: ' + response.message;
+    }).catch((err) => {
+      console.log(err);
+      this.linkGranterInfo = 'Unknown error: ' + JSON.stringify(err);
+      this.refreshGranters();
+    });
+  }
+
+  createNewGranter(form: FormGroup): void {
+    const granter = new Granter();
+    granter.id = this.newGranterId;
+    let isOk = false;
+
+    this._a2bbAuthService.post(Const.API_ENDPOINT + '/api/admin/granter', granter).then((res) => {
+      const response = res.json();
+
+      if (response.code === 0) {
+        this.newGranterInfo = 'Ok!';
+        isOk = true;
+        return this.refreshGranters();
+      }
+
+      this.newGranterInfo = 'Error: ' + response.message;
+
+      isOk = false;
+    }).then(() => {
+      if (isOk) {
+        form.reset();
+      }
+    }).catch((err) => {
+      console.log(err);
+      this.newGranterInfo = 'Unknown error: ' + JSON.stringify(err);
+    });
+  }
+
+  deleteSelectedUser(): void {
     if (!this.selectedUser) {
       return;
     }
@@ -81,6 +146,21 @@ export class DashboardComponent implements OnInit {
         this.selectedUser.id).then((res) => {
       this.selectedUser = null;
       return this.refreshUsers();
+    }).then(() => {
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  deleteSelectedGranter(): void {
+    if (!this.selectedGranter) {
+      return;
+    }
+
+    this._a2bbAuthService.delete(Const.API_ENDPOINT + '/api/admin/granter/' +
+        this.selectedGranter.id).then((res) => {
+      this.selectedGranter = null;
+      return this.refreshGranters();
     }).then(() => {
     }).catch((err) => {
       console.log(err);
